9934d25c403b88e1852749235d9c9a0be3dcd9b3
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-12-01 22:35:37 +0100

    Changes in templates

--- app/routes/session.py
+++ app/routes/session.py
@@ -42,7 +42,7 @@ def register():
                 flash('Account created successfully. Admin will contact you.', 'success')
                 
                 try:
-                    send_admin_email('New User', f'New user registered: {new_user.login}')
+                    send_admin_email('New User registered', f'New user has been registered in database.\n\nlogin: {new_user.login}\nname: {new_user.name}\nemail: {new_user.email}\ncreation_date: {new_user.creation_date}')
                 except Exception as e:
                     logger.error(f'Error sending registration email: {str(e)}')
                     flash('Registration was successful, but there was an error notifying the admin.', 'warning')
