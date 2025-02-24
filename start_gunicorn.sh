d885a91cbfbcb26520adf1d447715b5f9d1996af
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2025-02-24 23:16:31 +0100

    fix

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -2,3 +2,4 @@
 cd /home/pedro/StefanCryptoTradingBot
 source /home/pedro/StefanCryptoTradingBot/venv/bin/activate
 exec gunicorn -c /home/pedro/StefanCryptoTradingBot/gunicorn_config.py wsgi:app
+sudo ntpdate pool.ntp.org
