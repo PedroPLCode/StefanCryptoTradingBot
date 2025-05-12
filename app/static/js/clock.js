5662f926845b0c7a1c94edd9647fb97c151a706b
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2025-05-12 22:42:30 +0200

    Change clock js

--- app/static/js/clock.js
+++ app/static/js/clock.js
@@ -1,3 +1,32 @@
+function getFormattedDate(date) {
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  const formattedDate = new Intl.DateTimeFormat('en-CA', options).format(date);
+  const [year, month, day] = formattedDate.split('-');
+  return `${year}-${month}-${day}`;
+}
+
+function getShortTimezone() {
+  const options = { timeZoneName: "short" };
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  const parts = formatter.formatToParts(new Date());
+  return parts.find(p => p.type === "timeZoneName")?.value || "";
+}
+
+function updateClock() {
+  const hours = String(currentTime.getHours()).padStart(2, '0');
+  const minutes = String(currentTime.getMinutes()).padStart(2, '0');
+  const seconds = String(currentTime.getSeconds()).padStart(2, '0');
+  document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+  const formattedDateISO = getFormattedDate(currentTime);
+  document.getElementById('date').textContent = formattedDateISO;
+
+  const shortTimezone = getShortTimezone();
+  document.getElementById('timezone').textContent = shortTimezone;
+
+  currentTime.setSeconds(currentTime.getSeconds() + 1);
+}
+
 const serverTimeString = document.getElementById('server-time').textContent.trim();
 const serverTime = new Date(serverTimeString);
 
@@ -6,23 +35,6 @@ if (isNaN(serverTime)) {
 }
 
 let currentTime = new Date(serverTime.getTime());
-
-function updateClock() {
-  const hours = String(currentTime.getHours()).padStart(2, '0');
-  const minutes = String(currentTime.getMinutes()).padStart(2, '0');
-  const seconds = String(currentTime.getSeconds()).padStart(2, '0');
-  document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
-
-  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  
-  const date = new Date();
-  const options = { timeZoneName: "short" };
-  const shortTimezone = new Intl.DateTimeFormat("en-US", options).format(date);
-  
-  document.getElementById('timezone').textContent = `${shortTimezone}`;
-
-  currentTime.setSeconds(currentTime.getSeconds() + 1);
-}
 
 updateClock();
 setInterval(updateClock, 1000);
